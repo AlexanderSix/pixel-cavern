9c2599938233afb7649cef458b1853888fa9280d
Alexander Six <alexanderhsix@gmail.com>
2016-12-08 19:40:09 -0500

    Initial Commit

---
+++ pipe.js
@@ -0,0 +1,13 @@
+function Pipe() {
+  this.width = 40;
+  this.pipeTop = random(height/2);
+  this.pipeBottom = random(height/2);
+  this.enterLocation = width;
+  this.scrollSpeed = 2;
+
+  this.show = function() {
+    
+  }
+
+
+}
