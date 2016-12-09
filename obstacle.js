968225c41d450ba842b8c26606c3c6202437f48c
Alexander Six <alexanderhsix@gmail.com>
2016-12-08 20:01:44 -0500

    pipeTop and pipeBottom work around the center mark

--- obstacle.js
+++ obstacle.js
@@ -1,12 +1,14 @@
 function Obstacle() {
   this.width = 40;
-  this.pipeTop = random(height/2);
-  this.pipeBottom = random(height/2);
+  this.pipeBottom = random(100, height/2 - 40);
+  this.pipeTop = random(height/2 - 40, height-100);
   this.enterLocation = width;
   this.scrollSpeed = 2;
 
   this.show = function() {
-
+    fill(0, 255, 0);
+    rect(width/2, this.pipeTop, this.width, height);
+    rect(width/2, 0, this.width, this.pipeBottom);
   }
 
 
--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 var ship;
+var obstacle;
 
 function setup() {
   createCanvas(600, 600);
   ship = new Ship();
+  obstacle = new Obstacle();
 }
 
 function draw() {
   background(0);
   ship.show();
+  obstacle.show();
 }
